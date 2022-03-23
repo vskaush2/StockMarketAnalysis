c824928b01e75a3a94bee58ad49d1801b0ada2e1
Vivek Kaushik <vskaush2@illinois.edu>
2022-03-23 00:56:56 -0500

    Add files via upload

--- LeaderFollowerFrequencyAnalysis.py
+++ LeaderFollowerFrequencyAnalysis.py
@@ -1,6 +1,9 @@
 from CyclicityAnalysis import *
 from FetchPrices import *
 import os
+import pandas as pd
+from joblib import Parallel, delayed
+import itertools
 
 class LeaderFollowerFrequencyAnalysis:
 
@@ -11,83 +14,106 @@ class LeaderFollowerFrequencyAnalysis:
         self.end_date= end_date
         self.save_directory=save_directory
 
+
         if self.save_directory != None:
             try:
                 os.makedirs(self.save_directory)
             except:
                 pass
-
-        self.strong_leader_follower_relationship_days_df= self.get_strong_leader_follower_relationship_days_df(N=10,update=False)
-        self.strong_frequency_count_df=self.get_strong_frequency_count_df()
-
-    def get_topN_intraday_leader_follower_df(self, date, N=10, update=False):
-        topN_intraday_leader_follower_df = pd.DataFrame(columns=['Leader', 'Follower'])
-        file_path = "{}/{}.csv".format(self.save_directory, date)
-
-        if os.path.exists(file_path):
-            topN_intraday_leader_follower_df = pd.read_csv(file_path, index_col=0)
-        else:
-            update = True
-
-        if update:
-            try:
-                intraday_prices_df = FetchPrices(self.abbrevs,self.stock_type).fetch_intraday_prices_df(date)
-                CA = CyclicityAnalysis(intraday_prices_df)
-                top_pairs = CA.get_topN_leader_follower_pairs(N)
-                topN_intraday_leader_follower_df = pd.DataFrame(top_pairs, columns=['Leader', 'Follower'])
-                topN_intraday_leader_follower_df['Date'] = date
-                topN_intraday_leader_follower_df.set_index("Date", inplace=True)
-                print("SUCCESS OBTAINING TOP {} INTRADAY LEADER FOLLOWERS FOR {} ...".format(N, date))
-            except:
-                print("FAILED OBTAINING TOP {} INTRADAY LEADER FOLLOWERS FOR {} ...".format(N, date))
-
-            if self.save_directory != None:
-                topN_intraday_leader_follower_df.to_csv(file_path)
-
-        return topN_intraday_leader_follower_df
-
-    def get_strong_leader_follower_relationship_days_df(self,N=10,update=False):
-        date_range = pd.date_range(self.start_date, self.end_date, freq='D').strftime('%Y-%m-%d')
-        topN_intraday_leader_follower_dfs = [self.get_topN_intraday_leader_follower_df(date, N, update) for date in date_range]
-
-        concatenated_topN_intraday_leader_follower_df = pd.concat(topN_intraday_leader_follower_dfs, axis=0)
-        strong_leader_follower_relationship_days_df = concatenated_topN_intraday_leader_follower_df.value_counts().reset_index()
-        strong_leader_follower_relationship_days_df.rename(columns={0: 'Number of Strong Leader Follower Dates'},inplace=True)
-
-        leader_follower_pairs = strong_leader_follower_relationship_days_df[['Leader', 'Follower']].values
-
-        strong_leader_follower_dates = [concatenated_topN_intraday_leader_follower_df[(concatenated_topN_intraday_leader_follower_df['Leader'] == pair[0]) \
-                                            & (concatenated_topN_intraday_leader_follower_df['Follower'] == pair[-1])].index
-                                        for pair in leader_follower_pairs]
-
-        strong_leader_follower_relationship_days_df['Strong Leader Follower Dates'] = [list(x) for x in strong_leader_follower_dates]
-
-        return strong_leader_follower_relationship_days_df
-
-    def get_strong_frequency_count_df(self):
-
-        frequency_count_df = pd.DataFrame(columns= self.abbrevs, index= self.abbrevs)
-
-        for i in range(len(self.strong_leader_follower_relationship_days_df)):
-            leader, follower, frequency_count = self.strong_leader_follower_relationship_days_df.iloc[i]['Leader'], \
-                                                self.strong_leader_follower_relationship_days_df.iloc[i]['Follower'], \
-                                                self.strong_leader_follower_relationship_days_df.iloc[i]['Number of Strong Leader Follower Dates']
-
-            frequency_count_df.loc[leader, follower] = frequency_count # Filling nonzero counts first.
-
-        frequency_count_df.fillna(0, inplace=True) # All other counts are 0.
-        return frequency_count_df
-
-    def plot_strong_frequency_count_df(self, title='Strong Leader Follower Days Frequency Count Matrix',
-                                       color_label='Number of Strong Leader Follower Days',
-                                       color_continuous_scale='Bluered'):
-        fig = px.imshow(self.strong_frequency_count_df,
-                        title=title,
-                        labels=dict(color=color_label),
-                        x=self.strong_frequency_count_df.columns,
-                        y=self.strong_frequency_count_df.columns,
-                        color_continuous_scale=color_continuous_scale)
-        fig.update_xaxes(side='top')
-        fig.show()
+        self.strong_leader_follower_dates_df=self.get_strong_leader_follower_dates_df(N=10, update=False)
+        self.strong_frequency_counts_df=self.get_strong_frequency_counts_df(N=10, update=False)
 
 
+    def get_topN_intraday_leader_follower_pairs_df(self, date=today, N=10, update=False):
+      file_path = "{}/{}.csv".format(self.save_directory, date)
+      topN_intraday_leader_follower_pairs_df=None
+
+      if os.path.exists(file_path):
+        topN_intraday_leader_follower_pairs_df = pd.read_csv(file_path,index_col=0)
+
+      if os.path.exists(file_path) == False or date == today:
+        update=True
+
+      if update:
+        try:
+          intraday_prices_df=FetchPrices(self.abbrevs, self.stock_type).fetch_intraday_prices_df(date=date,preprocessed=True, parallelize=False)
+          intradayCA =CyclicityAnalysis(intraday_prices_df)
+          topN_intraday_leader_follower_pairs = intradayCA.get_topN_leader_follower_pairs(N)
+          topN_intraday_leader_follower_pairs_df=pd.DataFrame(topN_intraday_leader_follower_pairs, 
+          columns=['Leader','Follower'], index=[date]*N)
+          topN_intraday_leader_follower_pairs_df.index.rename('Date',inplace=True)
+        except:
+          topN_intraday_leader_follower_pairs_df=pd.DataFrame(columns=['Leader','Follower'])
+
+        if self.save_directory !=None:
+          topN_intraday_leader_follower_pairs_df.to_csv(file_path)
+
+      return topN_intraday_leader_follower_pairs_df
+
+    def get_strong_leader_follower_dates_df(self, N=10, update=False):
+      date_range = pd.date_range(self.start_date, self.end_date, freq='D').strftime("%Y-%m-%d")
+
+      topN_intraday_leader_follower_pairs_dfs = Parallel(n_jobs=len(date_range), verbose=0, prefer='threads')\
+      (delayed(self.get_topN_intraday_leader_follower_pairs_df)(date,N,update) for date in date_range)
+
+      concatenated_df=pd.concat(topN_intraday_leader_follower_pairs_dfs, axis=0)
+      strong_leader_follower_dates_df = concatenated_df.drop_duplicates(keep='first', ignore_index=True)
+      strong_dates_lists=[list(concatenated_df[(concatenated_df['Leader'] == pair[0]) & (concatenated_df['Follower'] == pair[-1])].index) \
+      for pair in strong_leader_follower_dates_df.values]
+
+      strong_leader_follower_dates_df['Strong Leader Follower Dates']= strong_dates_lists
+      strong_leader_follower_dates_df['Number of Strong Leader Follower Dates']=strong_leader_follower_dates_df['Strong Leader Follower Dates'].apply(lambda x: len(x))
+
+
+      return strong_leader_follower_dates_df
+
+    def get_strong_frequency_counts_df(self,N=10, update=False):
+      strong_leader_follower_dates_df = self.get_strong_leader_follower_dates_df(N=N, update=update)
+
+      strong_frequency_counts_df = pd.DataFrame(columns=self.abbrevs, index=self.abbrevs)
+      all_pairs=itertools.product(self.abbrevs, repeat= 2)
+
+      for pair in all_pairs:
+        try:
+          num_strong_leader_follower_dates = strong_leader_follower_dates_df[\
+          (strong_leader_follower_dates_df['Leader'] == pair[0]) \
+          & (strong_leader_follower_dates_df['Follower'] == pair[-1])]['Number of Strong Leader Follower Dates'].values[-1]
+        except:
+          num_strong_leader_follower_dates=0
+
+        strong_frequency_counts_df.loc[pair[0],pair[1]] = num_strong_leader_follower_dates
+      return strong_frequency_counts_df
+
+    def plot_strong_frequency_counts_df(self, title='Leader Follower Frequency Matrix', color_label='Number of Strong Leader Follower Dates', color_continuous_scale='viridis' ):
+      fig=px.imshow(self.strong_frequency_counts_df,
+                  title=title,
+                  labels=dict(color=color_label),
+                  x=self.strong_frequency_counts_df.columns,
+                  y=self.strong_frequency_counts_df.columns,
+                  color_continuous_scale=color_continuous_scale)
+      fig.show()
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      
+
+
+      
+
+
+      
+
+     
+
+    
+        
